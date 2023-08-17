da7918865b740d505ec1434525ebca6383430aaa
Álvaro González <alvgonz.ojeda@gmail.com>
2023-08-17 15:39:08 +0200

    [structure] added nextjs frontend structure

---
+++ frontend/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <main className="">
+            <header className="">
+                <h1>nextstudy</h1>
+            </header>
+        </main>
+    );
+}
