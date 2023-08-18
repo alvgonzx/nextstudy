692530a891af7d08867de4bb3e82e0bf51ffd11e
Álvaro González <alvgonz.ojeda@gmail.com>
2023-08-18 17:14:57 +0200

    [dev] updated ui

--- frontend/app/components/sidebar/Sidebar.jsx
+++ frontend/app/components/sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Icon from "./Icon";
+import Logo from "./Logo";
 
 const links = [
     {
@@ -36,7 +37,7 @@ const links = [
 
 const Sidebar = () => {
     return (
-        <aside className="border-r-2 border-[#AFAFAF] w-64 flex font-medium h-screen">
+        <aside className="border-r-2 border-[#AFAFAF] w-64 flex flex-col justify-between font-medium h-screen sticky top-0">
             <ul className="mt-3 ml-8">
                 {links.map(({ label, svg, route }) => (
                     <li key={route} className="py-3 flex gap-7">
@@ -45,6 +46,12 @@ const Sidebar = () => {
                     </li>
                 ))}
             </ul>
+            <div className="mx-auto">
+                <div className="flex flex-row items-center gap-3 pb-4">
+                    <Logo />
+                    <h1>nextstudy</h1>
+                </div>
+            </div>
         </aside>
     );
 };
--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,7 +13,6 @@ const RootLayout = ({ children }) => {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <Header />
                 <div className="flex">
                     <Sidebar />
                     {children}
