328b9128a24e81d76cbee8d465144d98b9669ed1
Álvaro González <alvgonz.ojeda@gmail.com>
2023-08-21 13:45:44 +0200

    [dev] added requestAI helper

---
+++ backend/helpers/requestAI.helper.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import OpenAI from 'openai';
+
+export const requestAI = async (system, prompt) => {
+	const openai = new OpenAI({
+		apiKey: process.env.OPENAI_API_KEY
+	});
+
+	const completion = await openai.chat.completions.create({
+		messages: [
+			{ role: 'system', content: system },
+			{ role: 'user', content: prompt }
+		],
+		model: 'gpt-3.5-turbo'
+	});
+
+	return completion.choices[0];
+};
