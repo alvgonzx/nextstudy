a2d783e4a121abd621c840594a707793137a5484
Álvaro González <alvgonz.ojeda@gmail.com>
2023-08-24 15:01:34 +0200

    [dev] added edit functionality

--- frontend/components/classrooms/ClassroomsForm.jsx
+++ frontend/components/classrooms/ClassroomsForm.jsx
@@ -1,19 +1,42 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LoadingIcon from "../LoadingIcon";
 
 const HomeworkForm = () => {
     const [name, setName] = useState("");
     const [professorName, setProfessorName] = useState("");
     const [professorEmail, setProfessorEmail] = useState("");
-    const [classrooms, setClassrooms] = useState();
-    
+    const [classrooms, setClassrooms] = useState([]);
+
+    const [isEditing, setIsEditing] = useState(null);
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/classrooms/")
+            .then((response) => response.json())
+            .then((classrooms) => setClassrooms(classrooms));
+    }, []);
+
+    const handleDelete = async (id) => {
+        await fetch(`http://localhost:5000/classrooms/${id}`, {
+            method: "DELETE",
+        });
+        const updatedClassrooms = classrooms.filter(
+            (classroom) => classroom.id !== id
+        );
+        setClassrooms(updatedClassrooms);
+    };
 
     let icon = "";
     if (loading) {
         icon = <LoadingIcon />;
+    }
+
+    const cleanInputs = () => {
+        setName("")
+        setProfessorName("")
+        setProfessorEmail("")
     }
 
     const handleNameChange = (event) => {
@@ -31,18 +54,18 @@ const HomeworkForm = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        let requestBody
+        let requestBody;
         if (!professorEmail) {
             requestBody = {
                 name: name,
                 professor_name: professorName,
-                professor_email: ''
+                professor_email: "",
             };
         } else {
             requestBody = {
                 name: name,
                 professor_name: professorName,
-                professor_email: professorEmail ? professorEmail : ''
+                professor_email: professorEmail ? professorEmail : "",
             };
         }
 
@@ -56,28 +79,109 @@ const HomeworkForm = () => {
                 body: JSON.stringify(requestBody),
             });
 
-            const classroom = await response.json()
+            const classroom = await response.json();
 
-            if(classroom.errors) {
-                setError(<p className="text-red-600">Your request has errors, make sure you have entered a valid email</p>)
+            if (classroom.errors) {
+                setError(
+                    <p className="text-red-600">
+                        Your request has errors, make sure you have entered a
+                        valid email
+                    </p>
+                );
                 setLoading(false);
                 return;
             }
 
-            setClassrooms(classroom);
-            setLoading(false);
+            setClassrooms((prevClassrooms) => [...prevClassrooms, classroom]);
             setError("");
         } catch (error) {
             setError(<p className="text-red-600">Internal server error</p>);
-            setLoading(false);
             console.error(error);
         }
+        setLoading(false);
+        cleanInputs();
+    };
+
+    const handleEditClick = (id) => {
+        setIsEditing(id);
+
+        const editedIndex = classrooms.findIndex(
+            (classroom) => classroom.id === id
+        );
+        
+        const classroom = classrooms[editedIndex]
+
+        setName(classroom.name);
+        setProfessorName(classroom.professor_name);
+        setProfessorEmail(classroom.professor_email);
+    };
+
+    const handleEditSubmit = async (event) => {
+        event.preventDefault();
+
+        let requestBody;
+        if (!professorEmail) {
+            requestBody = {
+                name: name,
+                professor_name: professorName,
+                professor_email: "",
+            };
+        } else {
+            requestBody = {
+                name: name,
+                professor_name: professorName,
+                professor_email: professorEmail ? professorEmail : "",
+            };
+        }
+
+        try {
+            setLoading(true);
+            const response = await fetch(
+                `http://localhost:5000/classrooms/${isEditing}`,
+                {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(requestBody),
+                }
+            );
+
+            const classroom = await response.json();
+
+            if (classroom.errors) {
+                setError(
+                    <p className="text-red-600">
+                        Your request has errors, make sure you have entered a
+                        valid email
+                    </p>
+                );
+                setLoading(false);
+                return;
+            }
+
+            const updatedClassrooms = [...classrooms];
+            const editedIndex = updatedClassrooms.findIndex(
+                (classroom) => classroom.id === isEditing
+            );
+            updatedClassrooms[editedIndex] = classroom;
+            setClassrooms(updatedClassrooms);
+
+            setError("");
+        } catch (error) {
+            setError(<p className="text-red-600">Internal server error</p>);
+            console.error(error);
+        }
+        
+        setIsEditing(null);
+        setLoading(false);
+        cleanInputs();
     };
 
     return (
         <>
             {error}
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={isEditing ? handleEditSubmit : handleSubmit}>
                 <div className="flex gap-3 w-2/3">
                     <div className="w-1/3">
                         <div className="mt-3">
@@ -142,9 +246,76 @@ const HomeworkForm = () => {
                     className="mt-2 text-white bg-[#D14141] hover:bg-red-700 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
                 >
                     {icon}
-                    Submit
+                    {isEditing ? "Edit" : "Submit"}
                 </button>
             </form>
+
+            <div className="relative overflow-x-auto border rounded-lg w-2/3 my-4">
+                <table className="w-full text-sm text-left text-gray-500">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                Classroom name
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Professor name
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Professor email
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                <span className="sr-only">Edit</span>
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                <span className="sr-only">Delete</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {classrooms.map((classroom) => (
+                            <tr
+                                className="bg-white hover:bg-gray-50"
+                                key={classroom.id}
+                            >
+                                <th
+                                    scope="row"
+                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                >
+                                    {classroom.name}
+                                </th>
+                                <td className="px-6 py-4">
+                                    {classroom.professor_name}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {classroom.professor_email
+                                        ? classroom.professor_email
+                                        : "Not added"}
+                                </td>
+                                <td className="px-6 py-4 text-right">
+                                    <button
+                                        className="font-medium text-blue-600 hover:underline"
+                                        onClick={() =>
+                                            handleEditClick(classroom.id)
+                                        }
+                                    >
+                                        Edit
+                                    </button>
+                                </td>
+                                <td className="px-6 py-4 text-right">
+                                    <button
+                                        className="font-medium text-red-600 hover:underline"
+                                        onClick={() =>
+                                            handleDelete(classroom.id)
+                                        }
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 };
