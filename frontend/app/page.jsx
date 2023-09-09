ad6b99f86e18f1664bc40f5e5973dba091ea92db
Alvaro Gonzalez <alvgonzx@Alvaros-Mac-Pro.local>
2023-09-09 12:05:47 +0200

    [dev] dashboard data from server

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -27,6 +27,19 @@ const getExams = async () => {
 	}
 };
 
+const getClassrooms = async () => {
+	try {
+		const res = await fetch('http://localhost:4000/classrooms/', { cache: 'no-store' });
+		if (!res.ok) {
+			return { response_error: true };
+		}
+
+		return res.json();
+	} catch {
+		return { response_error: true };
+	}
+};
+
 const HomePage = async () => {
 	const tasks = await getTasks();
 	const incompletedTasks = tasks.filter((task) => !task.completed);
@@ -34,11 +47,23 @@ const HomePage = async () => {
 	const exams = await getExams();
 	const remainingExams = exams.filter((exam) => exam.mark === undefined || exam.mark === null);
 
-	if (tasks.response_error || exams.response_error) {
+	const classrooms = await getClassrooms();
+
+	if (tasks.response_error || exams.response_error || classrooms.response_error) {
 		return (
 			<p className="text-red-600">Could not load data, are you sure you are running the backend?</p>
 		);
 	}
+
+	const classroomTasksMap = {};
+
+	tasks.forEach((task) => {
+		const { classroom_id, completed } = task;
+		if (!classroomTasksMap[classroom_id]) {
+			classroomTasksMap[classroom_id] = [];
+		}
+		if(!completed) classroomTasksMap[classroom_id].push(task);
+	});
 
 	return (
 		<>
@@ -50,16 +75,14 @@ const HomePage = async () => {
 
 			<p className="font-bold text-2xl mt-12">Active classrooms</p>
 			<section className="mt-5 flex flex-row gap-6 flex-wrap">
-				<SmallCard tasks={2} classroom="Maths" professor="John Doe" />
-				<SmallCard tasks={2} classroom="Maths" professor="John Doe" />
-				<SmallCard tasks={2} classroom="Maths" professor="John Doe" />
-				<SmallCard tasks={2} classroom="Maths" professor="John Doe" />
-				<SmallCard tasks={2} classroom="Maths" professor="John Doe" />
-				<SmallCard tasks={2} classroom="Maths" professor="John Doe" />
-				<SmallCard tasks={2} classroom="Maths" professor="John Doe" />
-				<SmallCard tasks={2} classroom="Maths" professor="John Doe" />
-				<SmallCard tasks={2} classroom="Maths" professor="John Doe" />
-				<SmallCard tasks={2} classroom="Maths" professor="John Doe" />
+				{classrooms.map((classroom) => (
+					<SmallCard
+						key={classroom.id}
+						classroom={classroom.name}
+						professor={classroom.professor_name}
+						tasks={classroomTasksMap[classroom.id] ? classroomTasksMap[classroom.id].length : 0}
+					/>
+				))}
 			</section>
 		</>
 	);
